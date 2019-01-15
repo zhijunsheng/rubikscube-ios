a864fef00808d6cb198fd02e34db4dcd6b427b23
xiaobao <xiaobao@xiaobaodeMacBook-Air.local>
2019-01-15 20:27:53 +0800

    we drew a grid of rubikscube

---
+++ RubiksCube/RubiksView.swift
@@ -0,0 +1,80 @@
+
+
+import UIKit
+
+class RubiksView: UIView {
+    let originX : CGFloat = 10
+    let originY : CGFloat = 10
+    let side    : CGFloat = 50
+    
+    override func draw(_ rect: CGRect) {
+        
+        drawSquare()
+        drawSquare()
+    
+    }
+    func drawSquare() {
+        let m14 = UIBezierPath()
+        // 1
+        // （1）
+        m14.move(to: CGPoint(x: originX, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX, y: originY + side))
+        m14.close()
+        // （2）
+        m14.move(to: CGPoint(x: originX + side, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
+        // （3）
+        m14.move(to: CGPoint(x: originX + side * 2, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
+        // 2
+        // （1）
+        m14.move(to: CGPoint(x: originX, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX, y: originY + side * 2))
+        m14.close()
+        // （2）
+        m14.move(to: CGPoint(x: originX + side, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
+        // （3）
+        m14.move(to: CGPoint(x: originX + side * 2, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
+        // 3
+        // （1）
+        m14.move(to: CGPoint(x: originX, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
+        m14.addLine(to: CGPoint(x: originX, y: originY + side * 3))
+        m14.close()
+        // （2）
+        m14.move(to: CGPoint(x: originX + side, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
+        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
+        // （3）
+        m14.move(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
+        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 3))
+        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
+        // Rubiks Cube‘s one ground
+        
+        m14.close()
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        m14.stroke()
+
+    }
+    
+
+    }
+
+
---
+++ RubiksCube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RubiksCube
+//
+//  Created by xiaobao on 2018/12/11.
+//  Copyright © 2018 xiaobao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
