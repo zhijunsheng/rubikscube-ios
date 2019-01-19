1a3dd094b287ac0a38b2bc3192ca17e97e99d8f2
xiaobao <xiaobao@xiaobaodeMacBook-Air.local>
2019-01-19 10:02:29 +0800

    we filled the first square

--- RubiksCube/RubiksView.swift
+++ RubiksCube/RubiksView.swift
@@ -6,6 +6,8 @@ class RubiksView: UIView {
     let originX : CGFloat = 10
     let originY : CGFloat = 10
     let side    : CGFloat = 50
+    let gap     : CGFloat = 5
+    
     
     override func draw(_ rect: CGRect) {
         
@@ -66,12 +68,19 @@ class RubiksView: UIView {
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 3))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
-        // Rubiks Cube‘s one ground
         
         m14.close()
         #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         m14.stroke()
-
+        
+        let n15 = UIBezierPath()
+        n15.move(to: CGPoint(x: originX + gap, y: originY + gap))
+        n15.addLine(to: CGPoint(x: originX + gap, y: originY + side - gap))
+        n15.addLine(to: CGPoint(x: originX + side - gap, y: originY + side - gap))
+        n15.addLine(to: CGPoint(x: originX + side - gap, y: originY + gap))
+        n15.close()
+        #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1).setFill()
+        n15.fill()
     }
     
 
