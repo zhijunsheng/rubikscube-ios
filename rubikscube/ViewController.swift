7d3153e3027ca938f215bfd296684653beeba40b
Zhijun Sheng <zhijunsheng@gmail.com>
2019-01-30 16:59:16 -0500

    created an empty xcode project

---
+++ rubikscube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rubikscube
+//
+//  Created by Donald Sheng on 2019-01-30.
+//  Copyright © 2019 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
