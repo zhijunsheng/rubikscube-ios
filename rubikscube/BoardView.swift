8743823409051a4d051bfbb601e64d13009afea7
ethan <ethanyiyaozhang@gmail.com>
2019-02-09 19:21:36 +0800

    we added lineWidth, fillColor, strokeColor.

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -4,11 +4,12 @@ import UIKit
  class BoardView: UIView {
 
     override func draw(_ rect: CGRect) {
-        drawSquare(originX: 50, originY: 0, cellSide: 90)
-        drawSquare(originX: 150, originY: 290, cellSide: 90)
+        drawSquare(originX: 50, originY: 0, cellSide: 90, lineWidth: 5, fillColor: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1), strokeColor: #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1))
+        drawSquare(originX: 150, originY: 290, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), strokeColor: #colorLiteral(red: 0.5058823824, green: 0.3372549117, blue: 0.06666667014, alpha: 1))
     }
     
-    func drawSquare(originX: CGFloat, originY: CGFloat, cellSide: CGFloat) {
+    // fillColor, strokeColor : UIColor
+    func drawSquare(originX: CGFloat, originY: CGFloat, cellSide: CGFloat, lineWidth: CGFloat, fillColor: UIColor, strokeColor: UIColor) {
         let pencil = UIBezierPath()
         
         pencil.move(to: CGPoint(x: originX, y: originY))
@@ -17,6 +18,10 @@ import UIKit
         pencil.addLine(to: CGPoint(x: originX, y: originY + cellSide))
         pencil.close()
         
+        pencil.lineWidth = lineWidth
+        fillColor.setFill()
+        pencil.fill()
+        strokeColor.setStroke()
         pencil.stroke()
     }
     
