445469cdd5452a8e6626dbc2c7a324e046dded65
ethan <ethanyiyaozhang@gmail.com>
2019-02-16 19:33:45 +0800

    drew 9 green parallelograms.

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -32,6 +32,34 @@ import UIKit
         drawParallelogram(originX: 150 + 20 + 20, originY: 250 - 40 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
         drawParallelogram(originX: 150 + 90 + 20 + 20, originY: 250 - 40 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
         drawParallelogram(originX: 150 + 90 + 90 + 20 + 20, originY: 250 - 40 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        drawParallelogram2(originX: 150 + 3 * 90, originY: 290, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 20, originY: 290 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 2 * 20, originY: 290 - 2 * 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        drawParallelogram2(originX: 150 + 3 * 90, originY: 290 + 90, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 20, originY: 290 + 90 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 2 * 20, originY: 290 + 90 - 2 * 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        
+        drawParallelogram2(originX: 150 + 3 * 90 , originY: 290 + 2 * 90, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 20, originY: 290 + 2 * 90 - 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        drawParallelogram2(originX: 150 + 3 * 90 + 2 * 20, originY: 290 + 2 * 90 - 2 * 40, cellSide: 90, lineWidth: 3, fillColor: #colorLiteral(red: 0, green: 0.9768045545, blue: 0, alpha: 1), strokeColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+    }
+    
+    func drawParallelogram2(originX: CGFloat, originY: CGFloat, cellSide: CGFloat, lineWidth: CGFloat, fillColor: UIColor, strokeColor: UIColor) {
+        let pencil = UIBezierPath()
+        
+        pencil.move(to: CGPoint(x: originX, y: originY))
+        pencil.addLine(to: CGPoint(x: originX + 20, y: originY - 40))
+        pencil.addLine(to: CGPoint(x: originX + 20, y: originY - 40 + cellSide))
+        pencil.addLine(to: CGPoint(x: originX, y: originY + cellSide))
+        pencil.close()
+        
+        pencil.lineWidth = lineWidth
+        fillColor.setFill()
+        pencil.fill()
+        strokeColor.setStroke()
+        pencil.stroke()
     }
     
     func drawParallelogram(originX: CGFloat, originY: CGFloat, cellSide: CGFloat, lineWidth: CGFloat, fillColor: UIColor, strokeColor: UIColor) {
