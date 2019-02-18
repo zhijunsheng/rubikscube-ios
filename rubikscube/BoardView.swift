9ec358bae153d86f740e0932563487777898e374
Lee Xu <leexu@Leeans-MacBook-Air.local>
2019-02-17 21:48:47 -0500

    We created the front face of a rubiks cube.

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -1,22 +1,31 @@
-//
-//  BoardView.swift
-//  rubikscube
-//
-//  Created by Lee Xu on 2019-02-17.
-//  Copyright © 2019 Gold Thumb Inc. All rights reserved.
-//
-
 import UIKit
 
 class BoardView: UIView {
-
-    
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    let originX: CGFloat = 50
+    let originY: CGFloat = 150
+    let side: CGFloat = 50
+   
     override func draw(_ rect: CGRect) {
-        // Drawing code
-        
+
+        for i in 0..<3 {
+            drawSquare(topLeftX: originX + CGFloat(i) * side, topLeftY: originY)
+            drawSquare(topLeftX: originX + CGFloat(i) * side, topLeftY: originY + side)
+            drawSquare(topLeftX: originX + CGFloat(i) * side, topLeftY: originY + side * 2)
+        }
     }
     
-
+    func drawSquare(topLeftX: CGFloat, topLeftY: CGFloat) {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: topLeftX, y: topLeftY))
+        path.addLine(to: CGPoint(x: topLeftX, y: topLeftY + side))
+        path.addLine(to: CGPoint(x: topLeftX + side, y: topLeftY + side))
+        path.addLine(to: CGPoint(x: topLeftX + side, y: topLeftY))
+        path.close()
+        
+        #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setFill()
+        path.fill()
+        
+        
+        path.stroke()
+    }
 }
