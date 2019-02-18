8dc0d656deed819ef4ae52805713fe3d61b2d7b7
Elaine <elaine@Elaines-MacBook.local>
2019-02-17 20:04:29 -0500

    Made a backround and boardview

---
+++ rubikscube/BoardView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardView.swift
+//  rubikscube
+//
+//  Created by Elaine on 2019-02-17.
+//  Copyright © 2019 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class BoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
