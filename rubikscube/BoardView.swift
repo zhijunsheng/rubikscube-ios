ed92a7708c5bb5a83a657f1a58f6a30c152ad68f
Elaine <elaine@Elaines-MacBook.local>
2019-02-19 20:14:06 -0500

    Made the second square to rubiks cube

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -12,11 +12,21 @@ class BoardView: UIView {
     override func draw(_ rect: CGRect) {
         
         let pencil = UIBezierPath()
+        
         pencil.move(to: CGPoint(x: 30, y: 100))
         pencil.addLine(to: CGPoint(x: 80, y: 100))
         pencil.addLine(to: CGPoint(x: 80, y: 150))
         pencil.addLine(to: CGPoint(x: 30, y: 150))
-        pencil.addLine(to: CGPoint(x: 30, y: 100))
+        pencil.close()
+     
+        pencil.move(to: CGPoint(x: 80, y: 100))
+        pencil.addLine(to: CGPoint(x: 130, y: 100))
+        pencil.addLine(to: CGPoint(x: 130, y: 150))
+        pencil.addLine(to: CGPoint(x: 80, y: 150))
+        pencil.close()
+        
+        
+        
         pencil.stroke()
     }
     
