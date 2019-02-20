c2429902123d824f1b1167064859d7eb4a3638e7
Elaine <elaine@Elaines-MacBook.local>
2019-02-19 21:04:29 -0500

    Replace everything with expressions :(

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
-class BoardView: UIView {
+class BoardView: UIView { // 37 instead of 50
     override func draw(_ rect: CGRect) {
         
         let pencil = UIBezierPath()
         
         pencil.move(to: CGPoint(x: 30, y: 100))
-        pencil.addLine(to: CGPoint(x: 80, y: 100))
-        pencil.addLine(to: CGPoint(x: 80, y: 150))
-        pencil.addLine(to: CGPoint(x: 30, y: 150))
+        pencil.addLine(to: CGPoint(x: 30 + 1 * 50, y: 100))
+        pencil.addLine(to: CGPoint(x: 30 + 1 * 50, y: 100 + 50))
+        pencil.addLine(to: CGPoint(x: 30, y: 100 + 50))
         pencil.close()
-     
-        pencil.move(to: CGPoint(x: 80, y: 100))
-        pencil.addLine(to: CGPoint(x: 130, y: 100))
-        pencil.addLine(to: CGPoint(x: 130, y: 150))
-        pencil.addLine(to: CGPoint(x: 80, y: 150))
+
+        pencil.move(to: CGPoint(x: 30 + 1 * 50, y: 100))
+        pencil.addLine(to: CGPoint(x: 30 + 2 * 50, y: 100))
+        pencil.addLine(to: CGPoint(x: 30 + 2 * 50, y: 100 + 50))
+        pencil.addLine(to: CGPoint(x: 30 + 1 * 50, y: 100 + 50))
         pencil.close()
+
+        pencil.move(to: CGPoint(x: 30 + 2 * 50, y: 100))
+        pencil.addLine(to: CGPoint(x: 30 + 3 * 50, y: 100))
+        pencil.addLine(to: CGPoint(x: 30 + 3 * 50, y: 100 + 50))
+        pencil.addLine(to: CGPoint(x: 30 + 2 * 50, y: 100 + 50))
+        pencil.close()
+        
+//        pencil.move(to: CGPoint(x: 30, y: 100))
+//        pencil.addLine(to: CGPoint(x: 30 + 1 * 50, y: 150))
+//        pencil.addLine(to: CGPoint(x: 30 + 1 * 50, y: 100 + 2 * 50))
+//        pencil.addLine(to: CGPoint(x: 30, y: 100 + 50))
+//        pencil.close()
         
         
         
