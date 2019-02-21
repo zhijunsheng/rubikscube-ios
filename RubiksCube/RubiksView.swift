0253b8a847b5469037f55fabd3cd07159be3934f
xiaobao <mariuspuzhouli@gmail.com>
2019-02-21 18:43:09 +0800

    we fixed the code shortly

--- RubiksCube/RubiksView.swift
+++ RubiksCube/RubiksView.swift
@@ -6,78 +6,30 @@ class RubiksView : UIView {
     let side     : CGFloat = 50
     let gap      : CGFloat = 5
 
-    
-    
     override func draw(_ rect: CGRect) {
         
-        drawSquare()
-        drawSquare()
+        drawSquares()
     
     }
-    func drawSquare() {
-        let m14 = UIBezierPath()
-        // 1/1
-        m14.move(to: CGPoint(x: originX, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX, y: originY + side))
-        m14.close()
-        // 1/2
-        m14.move(to: CGPoint(x: originX + side, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
-        // 1/3
-        m14.move(to: CGPoint(x: originX + side * 2, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
-        // 2/1
-        m14.move(to: CGPoint(x: originX, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX, y: originY + side * 2))
-        m14.close()
-        // 2/2
-        m14.move(to: CGPoint(x: originX + side, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
-        // 2/3
-        m14.move(to: CGPoint(x: originX + side * 2, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
-        // 3/1
-        m14.move(to: CGPoint(x: originX, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
-        m14.addLine(to: CGPoint(x: originX, y: originY + side * 3))
-        m14.close()
-        // 3/2
-        m14.move(to: CGPoint(x: originX + side, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
-        m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
-        // 3/3
-        m14.move(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
-        m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 3))
-        m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
+    
+    func drawSquares() {
         
-        m14.close()
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
-        m14.stroke()
-
         for f in 0..<3 {
             for i in 0..<3 {
                 drawRoundSquare(x: originX + gap + side * CGFloat(i), y: originY + gap + side * CGFloat(f))
             }
         }
-        
     }
     
     func drawRoundSquare(x: CGFloat, y: CGFloat) {
+        let m14 = UIBezierPath()
+        m14.move(to: CGPoint(x: x, y: y))
+        m14.addLine(to: CGPoint(x: x + side, y: y))
+        m14.addLine(to: CGPoint(x: x + side, y: y + side))
+        m14.addLine(to: CGPoint(x: x, y: y + side))
+        m14.close()
+        m14.stroke()
+        
         let n15 = UIBezierPath(roundedRect: CGRect(x: x, y: y, width: side - gap * 2, height: side - gap * 2), cornerRadius: 8)
         #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).setFill()
         n15.fill()
