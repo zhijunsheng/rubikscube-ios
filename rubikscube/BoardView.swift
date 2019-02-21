3d21af64adccf2907bd338287561ba9f9751906e
Lee Xu <leexu@Leeans-MacBook-Air.local>
2019-02-20 20:38:29 -0500

    We created side #2 of the rubiks cube and will be doing the third soon.

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -4,7 +4,8 @@ class BoardView: UIView {
     let originX: CGFloat = 50
     let originY: CGFloat = 150
     let side: CGFloat = 50
-   
+    let randomThingThatIDontKnowHowToName: CGFloat = 25
+
     override func draw(_ rect: CGRect) {
 
         for i in 0..<3 {
@@ -12,6 +13,12 @@ class BoardView: UIView {
             drawSquare(topLeftX: originX + CGFloat(i) * side, topLeftY: originY + side)
             drawSquare(topLeftX: originX + CGFloat(i) * side, topLeftY: originY + side * 2)
         }
+        for i in 0..<3 {
+            drawParallelogram(bottomLeftX: originX + CGFloat(i) * side, bottomLeftY: originY)
+            drawParallelogram(bottomLeftX: originX + CGFloat(i) * side + randomThingThatIDontKnowHowToName, bottomLeftY: originY - randomThingThatIDontKnowHowToName)
+            drawParallelogram(bottomLeftX: originX + CGFloat(i) * side + randomThingThatIDontKnowHowToName * 2, bottomLeftY: originY - randomThingThatIDontKnowHowToName * 2)
+        }
+        drawParallelogram2(cornerLeftX: originX + side * 3, cornerLeftY: originY)
     }
     
     func drawSquare(topLeftX: CGFloat, topLeftY: CGFloat) {
@@ -28,4 +35,39 @@ class BoardView: UIView {
         
         path.stroke()
     }
+    
+    
+    func drawParallelogram(bottomLeftX: CGFloat, bottomLeftY: CGFloat){
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: bottomLeftX, y: bottomLeftY))
+        path.addLine(to: CGPoint(x: bottomLeftX + 25, y: bottomLeftY - 25))
+        path.addLine(to: CGPoint(x: bottomLeftX + side + 25, y: bottomLeftY - 25))
+        path.addLine(to: CGPoint(x: bottomLeftX + side, y: bottomLeftY))
+        path.close()
+        
+        #colorLiteral(red: 0.9076325292, green: 1, blue: 0.1667297767, alpha: 1).setFill()
+        path.fill()
+        
+        
+        path.stroke()
+    }
+    
+    func drawParallelogram2(cornerLeftX: CGFloat, cornerLeftY: CGFloat){
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: cornerLeftX, y: cornerLeftY))
+        path.addLine(to: CGPoint(x: cornerLeftX, y: cornerLeftY + side))
+        path.addLine(to: CGPoint(x: cornerLeftX + side, y: cornerLeftY + side))
+        path.addLine(to: CGPoint(x: cornerLeftX + side, y: cornerLeftY))
+        path.close()
+        
+        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setFill()
+        path.fill()
+        
+        
+        path.stroke()
+    }
+    
 }
+
+
+
