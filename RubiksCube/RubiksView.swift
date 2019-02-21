ac36d7fe2bf9f946e01f56806475b46d77c159cb
xiaobao <mariuspuzhouli@gmail.com>
2019-02-21 18:10:37 +0800

    we changed the color of the front surface

--- RubiksCube/RubiksView.swift
+++ RubiksCube/RubiksView.swift
@@ -1,12 +1,11 @@
-
-
 import UIKit
 
-class RubiksView: UIView {
-    let originX : CGFloat = 10
-    let originY : CGFloat = 10
-    let side    : CGFloat = 50
-    let gap     : CGFloat = 5
+class RubiksView : UIView {
+    let originX  : CGFloat = 10
+    let originY  : CGFloat = 85
+    let side     : CGFloat = 50
+    let gap      : CGFloat = 5
+
     
     
     override func draw(_ rect: CGRect) {
@@ -17,53 +16,50 @@ class RubiksView: UIView {
     }
     func drawSquare() {
         let m14 = UIBezierPath()
-        // 1
-        // （1）
+        // 1/1
         m14.move(to: CGPoint(x: originX, y: originY))
         m14.addLine(to: CGPoint(x: originX + side, y: originY))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
         m14.addLine(to: CGPoint(x: originX, y: originY + side))
         m14.close()
-        // （2）
+        // 1/2
         m14.move(to: CGPoint(x: originX + side, y: originY))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
-        // （3）
+        // 1/3
         m14.move(to: CGPoint(x: originX + side * 2, y: originY))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
-        // 2
-        // （1）
+        // 2/1
         m14.move(to: CGPoint(x: originX, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX, y: originY + side * 2))
         m14.close()
-        // （2）
+        // 2/2
         m14.move(to: CGPoint(x: originX + side, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
-        // （3）
+        // 2/3
         m14.move(to: CGPoint(x: originX + side * 2, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
-        // 3
-        // （1）
+        // 3/1
         m14.move(to: CGPoint(x: originX, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
         m14.addLine(to: CGPoint(x: originX, y: originY + side * 3))
         m14.close()
-        // （2）
+        // 3/2
         m14.move(to: CGPoint(x: originX + side, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 2, y: originY + side * 3))
         m14.addLine(to: CGPoint(x: originX + side, y: originY + side * 3))
-        // （3）
+        // 3/3
         m14.move(to: CGPoint(x: originX + side * 2, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 2))
         m14.addLine(to: CGPoint(x: originX + side * 3, y: originY + side * 3))
@@ -79,15 +75,12 @@ class RubiksView: UIView {
             }
         }
         
-        
-        
     }
     
     func drawRoundSquare(x: CGFloat, y: CGFloat) {
         let n15 = UIBezierPath(roundedRect: CGRect(x: x, y: y, width: side - gap * 2, height: side - gap * 2), cornerRadius: 8)
-        #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1).setFill()
+        #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).setFill()
         n15.fill()
     }
+    
 }
-
-
