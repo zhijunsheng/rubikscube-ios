976904c27f4ed856d3ec6d662d66b71847ed8d23
daobao <lucashongyuanzhang@gmail.com>
2019-02-23 09:26:26 +0800

    we made a little square

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -3,6 +3,17 @@ import UIKit
  class BoardView: UIView {
 
     override func draw(_ rect: CGRect) {
+        drawSquare()
         
+    }
+    
+    func drawSquare() {
+        let cube = UIBezierPath()
+        cube.move(to: CGPoint(x: 35, y: 35))
+        cube.addLine(to: CGPoint(x: 75, y: 35))
+        cube.addLine(to: CGPoint(x: 75, y: 75))
+        cube.addLine(to: CGPoint(x: 35, y: 75))
+        cube.close()
+        cube.stroke()
     }
  }
