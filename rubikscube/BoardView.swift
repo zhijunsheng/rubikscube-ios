e161435970867a0f7b4e5395cfdbf1093ad1f67d
daobao <lucashongyuanzhang@gmail.com>
2019-02-24 10:05:43 +0800

    we have 3 squares

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -1,19 +1,30 @@
 import UIKit
 
  class BoardView: UIView {
-
+    let lineSide: CGFloat = 40
+    let oigX: CGFloat = 75
+    let oigY: CGFloat = 115
+    
     override func draw(_ rect: CGRect) {
-        drawSquare()
+        for i in 0..<3 {
+            drawSquare(x: oigX + CGFloat(i) * lineSide, y: oigY - lineSide, color: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
+        }
+        
+//        drawSquare(x: oigX, y: oigY - lineSide, color: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
+//        drawSquare(x: oigX + lineSide, y: oigY - lineSide, color: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0))
         
     }
     
-    func drawSquare() {
+    func drawSquare(x: CGFloat, y: CGFloat, color: UIColor) {
         let cube = UIBezierPath()
-        cube.move(to: CGPoint(x: 35, y: 35))
-        cube.addLine(to: CGPoint(x: 75, y: 35))
-        cube.addLine(to: CGPoint(x: 75, y: 75))
-        cube.addLine(to: CGPoint(x: 35, y: 75))
+        cube.move(to: CGPoint(x: x, y: y))
+        cube.addLine(to: CGPoint(x: x + lineSide, y: y))
+        cube.addLine(to: CGPoint(x: x + lineSide, y: y + lineSide))
+        cube.addLine(to: CGPoint(x: x, y: y + lineSide))
         cube.close()
+        color.setFill()
+        cube.fill()
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
         cube.stroke()
     }
  }
