751d8ae03484fbd60c99679265054c9bea427e58
Hal Wang <halhaoranwang@gmail.com>
2019-03-02 21:00:16 +0800

    drew up face

--- rubikscube/RubiksCubeView.swift
+++ rubikscube/RubiksCubeView.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class RubiksCubeView: UIView {
     let originX: CGFloat = 90
-    let originY: CGFloat = 100
+    let originY: CGFloat = 150
     let cellSide: CGFloat = 40
+    let offsetX: CGFloat = 20
+    let offsetY: CGFloat = 15
 
     override func draw(_ rect: CGRect) {
        
@@ -19,6 +21,10 @@ class RubiksCubeView: UIView {
             drawSquare(x: originX, y: originY + cellSide * CGFloat(i))
             drawSquare(x: originX + cellSide, y: originY + cellSide * CGFloat(i))
             drawSquare(x: originX + 2 * cellSide, y: originY + cellSide * CGFloat(i))
+            
+            drawParallelogram(x: originX + cellSide * CGFloat(i), y: originY)
+            drawParallelogram(x: originX + cellSide * CGFloat(i) + offsetX, y: originY - offsetY)
+            drawParallelogram(x: originX + cellSide * CGFloat(i) + offsetX * 2, y: originY - offsetY * 2)
         }
     }
 
@@ -29,11 +35,25 @@ class RubiksCubeView: UIView {
         pen.addLine(to: CGPoint(x: x + cellSide, y: y))
         pen.addLine(to: CGPoint(x: x + cellSide, y: y + cellSide))
         pen.addLine(to: CGPoint(x: x, y: y + cellSide))
+        
         pen.close()
         pen.stroke()
         
         UIColor.red.setFill()
         pen.fill()
     }
-
+    
+    func drawParallelogram(x: CGFloat, y:CGFloat) {
+        let pencil = UIBezierPath()
+        pencil.move(to: CGPoint(x: x, y: y))
+        pencil.addLine(to: CGPoint(x: x + offsetX, y: y - offsetY))
+        pencil.addLine(to: CGPoint(x: x + cellSide + offsetX, y: y - offsetY))
+        pencil.addLine(to: CGPoint(x: x + cellSide, y: y))
+        
+        pencil.close()
+        pencil.stroke()
+        
+        UIColor.yellow.setFill()
+        pencil.fill()
+    }
 }
