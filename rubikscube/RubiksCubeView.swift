c175ba123b3328c8cb27a2f24e17d3b04320082b
Hal Wang <halhaoranwang@gmail.com>
2019-03-02 20:07:48 +0800

    drew front face

--- rubikscube/RubiksCubeView.swift
+++ rubikscube/RubiksCubeView.swift
@@ -10,23 +10,30 @@ import UIKit
 
 class RubiksCubeView: UIView {
     let originX: CGFloat = 90
-    let originY: CGFloat = 39
-    let cellSide: CGFloat = 90
+    let originY: CGFloat = 100
+    let cellSide: CGFloat = 40
 
     override func draw(_ rect: CGRect) {
        
-        drawSquare()
+        for i in 0 ..< 3 {
+            drawSquare(x: originX, y: originY + cellSide * CGFloat(i))
+            drawSquare(x: originX + cellSide, y: originY + cellSide * CGFloat(i))
+            drawSquare(x: originX + 2 * cellSide, y: originY + cellSide * CGFloat(i))
+        }
     }
 
-    func drawSquare() {
+    func drawSquare(x: CGFloat, y: CGFloat) {
         let pen = UIBezierPath()
         
-        pen.move(to: CGPoint(x: originX, y: originY))
-        pen.addLine(to: CGPoint(x: originX + cellSide, y: originY))
-        pen.addLine(to: CGPoint(x: originX + cellSide, y: originY + cellSide))
-        pen.addLine(to: CGPoint(x: originX, y: originY + cellSide))
+        pen.move(to: CGPoint(x: x, y: y))
+        pen.addLine(to: CGPoint(x: x + cellSide, y: y))
+        pen.addLine(to: CGPoint(x: x + cellSide, y: y + cellSide))
+        pen.addLine(to: CGPoint(x: x, y: y + cellSide))
         pen.close()
         pen.stroke()
+        
+        UIColor.red.setFill()
+        pen.fill()
     }
 
 }
