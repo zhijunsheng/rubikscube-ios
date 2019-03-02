6567d45241c58e36fd310238cd49dd67831c4517
xiaobao <mariuspuzhouli@gmail.com>
2019-03-02 10:44:08 +0800

    we had finished "RubiksCube"

--- RubiksCube/RubiksView.swift
+++ RubiksCube/RubiksView.swift
@@ -2,36 +2,63 @@ import UIKit
 
 class RubiksView : UIView {
     let originX  : CGFloat = 10
-    let originY  : CGFloat = 85
+    let originY  : CGFloat = 150
     let side     : CGFloat = 50
-//    let gap      : CGFloat = 5
 
     override func draw(_ rect: CGRect) {
         
-        drawSquares()
-    
-    }
-    
-    func drawSquares() {
-        
-        for f in 0..<3 {
-            for i in 0..<3 {
-                drawRoundSquare(x: originX + side * CGFloat(i), y: originY + side * CGFloat(f))
-            }
+        for i in 0..<3 {
+            drawSquare(x: originX + side * CGFloat(i), y: originY + side * 0)
+            drawSquare(x: originX + side * CGFloat(i), y: originY + side * 1)
+            drawSquare(x: originX + side * CGFloat(i), y: originY + side * 2)
+            drawParallelogram1(bottomLeftX: originX + side * CGFloat(i) + 0 * side / 4, bottomLeftY: originY)
+            drawParallelogram1(bottomLeftX: originX + side * CGFloat(i) + 1 * side / 4, bottomLeftY: originY - side / 2)
+            drawParallelogram1(bottomLeftX: originX + side * CGFloat(i) + 2 * side / 4, bottomLeftY: originY - side)
+            drawParallelogram2(bottomLeftX: originX + side * 3, bottomLeftY: originY + side * CGFloat(i) + side)
+            drawParallelogram2(bottomLeftX: originX + side / 4 + side * 3, bottomLeftY: originY + side / 2 + side * CGFloat(i))
+            drawParallelogram2(bottomLeftX: originX + side / 2 + side * 3, bottomLeftY: originY + side * CGFloat(i))
         }
+        
     }
     
-    func drawRoundSquare(x: CGFloat, y: CGFloat) {
+    func drawParallelogram1(bottomLeftX: CGFloat, bottomLeftY: CGFloat) {
+        let a = UIBezierPath()
+        a.move(to: CGPoint(x: bottomLeftX, y: bottomLeftY))
+        a.addLine(to: CGPoint(x: bottomLeftX + side, y: bottomLeftY))
+        a.addLine(to: CGPoint(x: bottomLeftX + side + side / 4, y: bottomLeftY - side / 2))
+        a.addLine(to: CGPoint(x: bottomLeftX + side + side / 4 - side, y: bottomLeftY - side / 2))
+        a.close()
+        a.lineWidth = 1
+        a.stroke()
+        #colorLiteral(red: 0.9843137255, green: 1, blue: 0, alpha: 1).setFill()
+        a.fill()
+        
+    }
+    
+    func drawParallelogram2(bottomLeftX: CGFloat, bottomLeftY: CGFloat) {
+        let s = UIBezierPath()
+        s.move(to: CGPoint(x: bottomLeftX, y: bottomLeftY))
+        s.addLine(to: CGPoint(x: bottomLeftX, y: bottomLeftY - side))
+        s.addLine(to: CGPoint(x: bottomLeftX + side / 4, y: bottomLeftY - side / 2 - side))
+        s.addLine(to: CGPoint(x: bottomLeftX + side / 4, y: bottomLeftY - side / 2 - side + side))
+        s.close()
+        s.lineWidth = 1
+        s.stroke()
+        #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1).setFill()
+        s.fill()
+        
+    }
+    
+    func drawSquare(x: CGFloat, y: CGFloat) {
         let m14 = UIBezierPath()
         m14.move(to: CGPoint(x: x, y: y))
         m14.addLine(to: CGPoint(x: x + side, y: y))
         m14.addLine(to: CGPoint(x: x + side, y: y + side))
         m14.addLine(to: CGPoint(x: x, y: y + side))
         m14.close()
-        m14.lineWidth = 3
+        m14.lineWidth = 1
         m14.stroke()
         
-//        let n15 = UIBezierPath(roundedRect: CGRect(x: x, y: y, width: side - gap * 2, height: side - gap * 2), cornerRadius: 8)
         #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).setFill()
         m14.fill()
     }
