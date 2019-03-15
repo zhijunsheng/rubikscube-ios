7f1b18adba763b558b05b15c3988c3bc477ec928
Hal Wang <halhaoranwang@gmail.com>
2019-03-15 20:38:43 +0800

    refactored the rotateU

--- rubikscube/RubiksCube.swift
+++ rubikscube/RubiksCube.swift
@@ -17,25 +17,12 @@ struct RubiksCube {
     var back: [CellColor]
     
     mutating func rotateU() {
-        let front0 = front[0]
-        let front1 = front[1]
-        let front2 = front[2]
-        
-        front[0] = right[0]
-        front[1] = right[1]
-        front[2] = right[2]
-        
-        right[0] = back[0]
-        right[1] = back[1]
-        right[2] = back[2]
-        
-        back[0] = left[0]
-        back[1] = left[1]
-        back[2] = left[2]
-        
-        left[0] = front0
-        left[1] = front1
-        left[2] = front2
-        
+        for i in 0 ..< 3 {
+            let tmp = front[i]
+            front[i] = right[i]
+            right[i] = back[i]
+            back[i] = left[i]
+            left[i] = tmp
+        }
     }
 }
