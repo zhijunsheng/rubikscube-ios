d34d673cc0f640fc74557da7cc586fe070424d15
Peter Shi <petershi@Peters-MacBook-Pro-10.local>
2019-08-04 20:26:11 -0400

    kind of fixed cube

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -11,8 +11,8 @@ import UIKit
  class BoardView: UIView {
     
     let side: CGFloat = 100
-    let parallelogramSide: CGFloat = 75
-    let parallelogramSide2: CGFloat = 70
+//    let parallelogramSide: CGFloat = 50
+//    let parallelogramSide2: CGFloat = 75
     let cornerX: CGFloat = 125
     let cornerY: CGFloat = 375
     
@@ -37,25 +37,26 @@ import UIKit
         drawPiece(x: 1, y: 2, color: board.red[7])
         drawPiece(x: 2, y: 2, color: board.red[8])
         
-        drawPieceParallelogram(x: 0, y: 0, color: board.blue[0])
-        drawPieceParallelogram(x: 1, y: 0, color: board.blue[1])
-        drawPieceParallelogram(x: 2, y: 0, color: board.blue[2])
-        drawPieceParallelogram(x: 0, y: 1, color: board.blue[3])
-        drawPieceParallelogram(x: 1, y: 1, color: board.blue[4])
-        drawPieceParallelogram(x: 2, y: 1, color: board.blue[5])
-        drawPieceParallelogram(x: 0, y: 2, color: board.blue[6])
-        drawPieceParallelogram(x: 1, y: 2, color: board.blue[7])
-        drawPieceParallelogram(x: 2, y: 2, color: board.blue[8])
+        drawPieceParallelogram(x: 0, y: 0, color: board.white[0])
+        drawPieceParallelogram(x: 1, y: 0, color: board.white[1])
+        drawPieceParallelogram(x: 2, y: 0, color: board.white[2])
+        drawPieceParallelogram(x: 0, y: 1, color: board.white[3])
+        drawPieceParallelogram(x: 1, y: 1, color: board.white[4])
+        drawPieceParallelogram(x: 2, y: 1, color: board.white[5])
+        drawPieceParallelogram(x: 0, y: 2, color: board.white[6])
+        drawPieceParallelogram(x: 1, y: 2, color: board.white[7])
+        drawPieceParallelogram(x: 2, y: 2, color: board.white[8])
         
-        drawPieceParallelogram2(x: 0, y: 0, color: board.white[0])
-        drawPieceParallelogram2(x: 1, y: 0, color: board.white[1])
-        drawPieceParallelogram2(x: 2, y: 0, color: board.white[2])
-        drawPieceParallelogram2(x: 0, y: 1, color: board.white[3])
-        drawPieceParallelogram2(x: 1, y: 1, color: board.white[4])
-        drawPieceParallelogram2(x: 2, y: 1, color: board.white[5])
-        drawPieceParallelogram2(x: 0, y: 2, color: board.white[6])
-        drawPieceParallelogram2(x: 1, y: 2, color: board.white[7])
-        drawPieceParallelogram2(x: 2, y: 2, color: board.white[8])
+        drawPieceParallelogram2(x: 0, y: 0, color: board.blue[0])
+        drawPieceParallelogram2(x: 1, y: 0, color: board.blue[1])
+        drawPieceParallelogram2(x: 2, y: 0, color: board.blue[2])
+        drawPieceParallelogram2(x: 0, y: 1, color: board.blue[3])
+        drawPieceParallelogram2(x: 1, y: 1, color: board.blue[4])
+        drawPieceParallelogram2(x: 2, y: 1, color: board.blue[5])
+        drawPieceParallelogram2(x: 0, y: 2, color: board.blue[6])
+        drawPieceParallelogram2(x: 1, y: 2, color: board.blue[7])
+        drawPieceParallelogram2(x: 2, y: 2, color: board.blue[8])
+        
     }
     
     func drawPiece(x: Int, y: Int, color: String) {
@@ -64,6 +65,82 @@ import UIKit
         pencil.addLine(to: CGPoint(x: cornerX + side * CGFloat(x), y: cornerY + side * CGFloat(y) + side))
         pencil.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y) + side))
         pencil.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y)))
+        pencil.close()
+
+        if color == "W" {
+            UIColor.white.setFill()
+        } else if color == "R" {
+            UIColor.red.setFill()
+        } else if color == "B" {
+            UIColor.blue.setFill()
+        } else if color == "G" {
+            UIColor.green.setFill()
+        } else if color == "Y" {
+            UIColor.yellow.setFill()
+        } else if color == "O" {
+            UIColor.orange.setFill()
+        } else {
+            UIColor.groupTableViewBackground.setFill()
+        }
+//        pencil.fill()
+        
+        let pen = UIBezierPath()
+        pen.move(to: CGPoint(x: cornerX + side * CGFloat(x), y: cornerY + side * CGFloat(y)))
+        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x), y: cornerY + side * CGFloat(y) + side))
+        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y) + side))
+        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y)))
+        pen.close()
+        
+        UIColor.black.setStroke()
+        
+        pencil.stroke()
+    }
+    
+    func drawPieceParallelogram2(x: Int, y: Int, color: String) {
+        let cornerXP: CGFloat = cornerX + side * 3 // 425
+        let cornerYP: CGFloat = cornerY
+        
+        let pencil = UIBezierPath()
+        
+        pencil.move(to: CGPoint(x: cornerXP + 0.65 * side * CGFloat(x), y: cornerYP + side * CGFloat(y) - CGFloat(x) * 0.65 * side))
+        pencil.addLine(to: CGPoint(x: cornerXP + 0.65 * side * CGFloat(x), y: cornerYP + side * CGFloat(y) + 100 - CGFloat(x) * 0.65 * side))
+        pencil.addLine(to: CGPoint(x: cornerXP + 0.65 * side * CGFloat(x) + 50, y: cornerYP + side * CGFloat(y) + 25 - CGFloat(x) * 0.65 * side))
+        pencil.addLine(to: CGPoint(x: cornerXP + 0.65 * side * CGFloat(x) + 50, y: cornerYP + side * CGFloat(y) - 75 - CGFloat(x) * 0.65 * side))
+        pencil.close()
+        
+        if color == "W" {
+            UIColor.white.setFill()
+        } else if color == "R" {
+            UIColor.red.setFill()
+        } else if color == "B" {
+            UIColor.blue.setFill()
+        } else if color == "G" {
+            UIColor.green.setFill()
+        } else if color == "Y" {
+            UIColor.yellow.setFill()
+        } else if color == "O" {
+            UIColor.orange.setFill()
+        } else {
+            UIColor.groupTableViewBackground.setFill()
+        }
+//        pencil.fill()
+        
+        UIColor.black.setStroke()
+        pencil.stroke()
+        
+
+    }
+    
+    func drawPieceParallelogram(x: Int, y: Int, color: String) {
+        let cornerXP: CGFloat = 335
+        let cornerYP: CGFloat = 150
+        
+        let pencil = UIBezierPath()
+        
+        pencil.move(to: CGPoint(x: cornerXP + side * CGFloat(x) - CGFloat(y * 70), y: cornerYP + side * 0.65 * CGFloat(y)))
+        pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 70 - CGFloat(y * 70), y: cornerYP + side * 0.65 * CGFloat(y) + 75))
+        pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 40 - CGFloat(y * 70), y: cornerYP + side * 0.65 * CGFloat(y) + 75))
+        pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 115 - CGFloat(y * 70), y: cornerYP + side * 0.65 * CGFloat(y)))
         pencil.close()
         
         if color == "W" {
@@ -83,159 +160,8 @@ import UIKit
         }
         pencil.fill()
         
-        let pen = UIBezierPath()
-        pen.move(to: CGPoint(x: cornerX + side * CGFloat(x), y: cornerY + side * CGFloat(y)))
-        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x), y: cornerY + side * CGFloat(y) + side))
-        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y) + side))
-        pen.addLine(to: CGPoint(x: cornerX + side * CGFloat(x) + side, y: cornerY + side * CGFloat(y)))
-        pen.close()
-        
-        UIColor.black.setStroke()
-        
         pencil.stroke()
+        
     }
-    
-    func drawPieceParallelogram(x: Int, y: Int, color: String) {
-        let cornerXP: CGFloat = 425
-        let cornerYP: CGFloat = 375
-        
-        // x 0
-        // x 1 should go higher
-        // x 2 should go even higher
-        let pencil = UIBezierPath()
-        
-        if x == 0 {
-            pencil.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y)))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) + 100))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) + 25))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 75))
-            pencil.close()
-        } else if x == 1 {
-            pencil.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) + 25))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 50))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 150))
-            pencil.close()
-        } else if x == 2 {
-            pencil.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 150))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 50))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 125))
-            pencil.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 225))
-            pencil.close()
-        }
-        
-        if color == "W" {
-            UIColor.white.setFill()
-        } else if color == "R" {
-            UIColor.red.setFill()
-        } else if color == "B" {
-            UIColor.blue.setFill()
-        } else if color == "G" {
-            UIColor.green.setFill()
-        } else if color == "Y" {
-            UIColor.yellow.setFill()
-        } else if color == "O" {
-            UIColor.orange.setFill()
-        } else {
-            UIColor.groupTableViewBackground.setFill()
-        }
-        pencil.fill()
-        
-        let pen = UIBezierPath()
-        
-        if x == 0 {
-            pen.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y)))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) + 100))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) + 25))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 75))
-            pen.close()
-        } else if x == 1 {
-            pen.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 75))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) + 25))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 50))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 150))
-            pen.close()
-        } else if x == 2 {
-            pen.move(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 150))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x), y: cornerYP + side * CGFloat(y) - 50))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 125))
-            pen.addLine(to: CGPoint(x: cornerXP + parallelogramSide * CGFloat(x) + 75, y: cornerYP + side * CGFloat(y) - 225))
-            pen.close()
-        }
-        UIColor.black.setStroke()
-        
-        pen.stroke()
-    }
-    
-    func drawPieceParallelogram2(x: Int, y: Int, color: String) {
-        let cornerXP: CGFloat = 335
-        let cornerYP: CGFloat = 150
-        
-        let pencil = UIBezierPath()
-        
-        if y == 0 {
-            pencil.move(to: CGPoint(x: cornerXP + side * CGFloat(x), y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 70, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 40, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 115, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.close()
-        } else if y == 1 {
-            pencil.move(to: CGPoint(x: cornerXP + side * CGFloat(x) - 70, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 140, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 35, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 40, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.close()
-        } else if y == 2 {
-            pencil.move(to: CGPoint(x: cornerXP + side * CGFloat(x) - 140, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 210, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 110, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pencil.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 35, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pencil.close()
-        }
-        
-        if color == "W" {
-            UIColor.white.setFill()
-        } else if color == "R" {
-            UIColor.red.setFill()
-        } else if color == "B" {
-            UIColor.blue.setFill()
-        } else if color == "G" {
-            UIColor.green.setFill()
-        } else if color == "Y" {
-            UIColor.yellow.setFill()
-        } else if color == "O" {
-            UIColor.orange.setFill()
-        } else {
-            UIColor.groupTableViewBackground.setFill()
-        }
-        pencil.fill()
-        
-        let pen = UIBezierPath()
-        
-        if y == 0 {
-            pen.move(to: CGPoint(x: cornerXP + side * CGFloat(x), y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 70, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 40, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 115, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.close()
-        } else if y == 1 {
-            pen.move(to: CGPoint(x: cornerXP + side * CGFloat(x) - 70, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 140, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 35, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) + 40, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.close()
-        } else if y == 2 {
-            pen.move(to: CGPoint(x: cornerXP + side * CGFloat(x) - 140, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 210, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 110, y: cornerYP + parallelogramSide * CGFloat(y) + 75))
-            pen.addLine(to: CGPoint(x: cornerXP + side * CGFloat(x) - 35, y: cornerYP + parallelogramSide * CGFloat(y)))
-            pen.close()
-        }
-        
-        UIColor.black.setStroke()
-        
-        pen.stroke()
-    }
-    
     
  }
