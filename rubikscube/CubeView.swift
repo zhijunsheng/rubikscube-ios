a98681522cc70007c759f9c0771aa0b6ed20b6a3
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-01-26 20:10:43 -0600

    made non color cube

--- rubikscube/CubeView.swift
+++ rubikscube/CubeView.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class CubeView: UIView {
     override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-        path.move(to: CGPoint(x: 100, y: 100))
-        path.addLine(to: CGPoint(x: 400, y: 100))
+        let originX: CGFloat = 100
+        let originY: CGFloat = 200
+        let leftPointX: CGFloat = 225
+        let rightPointX: CGFloat = 675
+        let pointY: CGFloat = 80
+        let squareSide: CGFloat = 450 / 3
+        
+        let path = UIBezierPath(rect: CGRect(x: originX, y: originY, width: squareSide * 3, height: squareSide * 3))
+        path.move(to: CGPoint(x: originX, y: originY))
+        path.addLine(to: CGPoint(x: leftPointX, y: pointY))
+        path.move(to: CGPoint(x: originX + squareSide * 3, y: originY))
+        path.addLine(to: CGPoint(x: rightPointX, y: pointY))
+        path.addLine(to: CGPoint(x: leftPointX, y: pointY))
+        path.move(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide * 3))
+        path.addLine(to: CGPoint(x: rightPointX, y: pointY + squareSide * 3))
+        path.addLine(to: CGPoint(x: rightPointX, y: pointY))
+        
+        path.move(to: CGPoint(x: originX + squareSide, y: originY))
+        path.addLine(to: CGPoint(x: originX + squareSide, y: originY + squareSide * 3))
+        path.move(to: CGPoint(x: originX + squareSide * 2, y: originY))
+        path.addLine(to: CGPoint(x: originX + squareSide * 2, y: originY + squareSide * 3))
+        path.move(to: CGPoint(x: originX, y: originY + squareSide))
+        path.addLine(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide))
+        path.move(to: CGPoint(x: originX, y: originY + squareSide * 2))
+        path.addLine(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide * 2))
+
+        
         path.stroke()
     }
 }
