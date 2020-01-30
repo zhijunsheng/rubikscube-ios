3a29733b37b0524088a704c24cf2a2b13025351f
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-01-30 15:49:09 -0600

    added top lines and organized code

--- rubikscube/CubeView.swift
+++ rubikscube/CubeView.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class CubeView: UIView {
+    let squareSide: CGFloat = 104// 150
+    let originX: CGFloat = 150
+    let originY: CGFloat = 300
+    var leftPointX: CGFloat = 2256546 - 23456789
+    var rightPointX: CGFloat = 676879
+    var pointY: CGFloat = 806997
+
     override func draw(_ rect: CGRect) {
-        let originX: CGFloat = 100
-        let originY: CGFloat = 200
-        let leftPointX: CGFloat = 225
-        let rightPointX: CGFloat = 675
-        let pointY: CGFloat = 80
-        let squareSide: CGFloat = 450 / 3
-        
+        leftPointX = originX + 125
+        rightPointX = originX + squareSide * 3 + 125
+        pointY = originY - 120
         let path = UIBezierPath(rect: CGRect(x: originX, y: originY, width: squareSide * 3, height: squareSide * 3))
+        
+        drawCubeEdges()
+        drawFront()
+        drawTop()
+        drawRight()
+        path.stroke()
+    }
+    
+    func drawCubeEdges() {
+        let path = UIBezierPath()
         path.move(to: CGPoint(x: originX, y: originY))
         path.addLine(to: CGPoint(x: leftPointX, y: pointY))
         path.move(to: CGPoint(x: originX + squareSide * 3, y: originY))
@@ -26,7 +39,10 @@ class CubeView: UIView {
         path.move(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide * 3))
         path.addLine(to: CGPoint(x: rightPointX, y: pointY + squareSide * 3))
         path.addLine(to: CGPoint(x: rightPointX, y: pointY))
-        
+        path.stroke()
+    }
+    func drawFront() {
+        let path = UIBezierPath()
         path.move(to: CGPoint(x: originX + squareSide, y: originY))
         path.addLine(to: CGPoint(x: originX + squareSide, y: originY + squareSide * 3))
         path.move(to: CGPoint(x: originX + squareSide * 2, y: originY))
@@ -35,8 +51,31 @@ class CubeView: UIView {
         path.addLine(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide))
         path.move(to: CGPoint(x: originX, y: originY + squareSide * 2))
         path.addLine(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide * 2))
-
+        path.stroke()
+    }
+    
+    func drawTop() {
+        let deltaX: CGFloat = (leftPointX - originX) / 3
+        let deltaY: CGFloat = (pointY - originY) / 3
+        let path = UIBezierPath()
         
+        path.move(to: CGPoint(x: originX + squareSide, y: originY))
+        path.addLine(to: CGPoint(x: leftPointX + squareSide, y: pointY))
+        path.move(to: CGPoint(x: originX + squareSide * 2, y: originY))
+        path.addLine(to: CGPoint(x: leftPointX + squareSide * 2, y: pointY))
+        path.move(to: CGPoint(x: originX + deltaX, y: originY + deltaY))
+        path.addLine(to: CGPoint(x: originX + deltaX + squareSide * 3, y: originY + deltaY))
+        path.move(to: CGPoint(x: originX + deltaX * 2, y: originY + deltaY * 2))
+        path.addLine(to: CGPoint(x: originX + deltaX * 2 + squareSide * 3, y: originY + deltaY * 2))
+
+        path.stroke()
+    }
+    func drawRight() {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide))
+        path.addLine(to: CGPoint(x: rightPointX, y: pointY + squareSide))
+        path.move(to: CGPoint(x: originX + squareSide * 3, y: originY + squareSide * 2))
+        path.addLine(to: CGPoint(x: rightPointX, y: pointY + squareSide * 2))
         path.stroke()
     }
 }
