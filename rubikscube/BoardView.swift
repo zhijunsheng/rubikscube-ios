dc9c2cbe761419445b0197274b028654e0b45690
daobao <lucashongyuanzhang@gmail.com>
2020-03-11 18:48:33 +0800

    wwe have a full rubikscube

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -6,10 +6,16 @@ class BoardView: UIView {
     var oigY: CGFloat = 295
     
     let frontFace: [CubeColor] = [.green, .green, .green, .green, .green, .green, .green, .green, .green]
-    let rightFace: [CubeColor] = [.red, .orange, .yellow, .white, .blue, .green, .red, .yellow, .blue]
+    let rightFace: [CubeColor] = [.white, .white, .white, .white, .white, .white, .white, .white, .white]
+    let topFace: [CubeColor] = [.red, .red, .red, .red, .red, .red, .red, .red, .red]
     
     override func draw(_ rect: CGRect) {
-       // front face
+        drawFrontFace()
+        drawRightFace()
+        drawTopface()
+    }
+    
+    func drawFrontFace() {
         drawSquare(x: 0, y: 0, color: frontFace[0])
         drawSquare(x: 1, y: 0, color: frontFace[1])
         drawSquare(x: 2, y: 0, color: frontFace[2])
@@ -21,7 +27,9 @@ class BoardView: UIView {
         drawSquare(x: 0, y: 2, color: frontFace[6])
         drawSquare(x: 1, y: 2, color: frontFace[7])
         drawSquare(x: 2, y: 2, color: frontFace[8])
-        
+    }
+    
+    func drawRightFace() {
         drawRightParallelogram(x: 0, y: 0, color: rightFace[0])
         drawRightParallelogram(x: 1, y: 0, color: rightFace[1])
         drawRightParallelogram(x: 2, y: 0, color: rightFace[2])
@@ -33,18 +41,15 @@ class BoardView: UIView {
         drawRightParallelogram(x: 0, y: 2, color: rightFace[6])
         drawRightParallelogram(x: 1, y: 2, color: rightFace[7])
         drawRightParallelogram(x: 2, y: 2, color: rightFace[8])
-        
     }
-    
-    
-    
+   
     func drawSquare(x: Int, y: Int, color: CubeColor) {
         let cube0 = UIBezierPath(rect: CGRect(x: oigX + CGFloat(x) * lineSide, y: oigY + CGFloat(y) * lineSide, width: lineSide, height: lineSide))
         changingColor(color: color).setFill()
         cube0.fill()
         cube0.stroke()
     }
-    
+ 
     func drawRightParallelogram(x: Int, y: Int, color: CubeColor) {
         let wrongPointX1 = oigX + 3 * lineSide + CGFloat(x) / 2 * lineSide
         let wrongPointY1 = oigY - CGFloat(x) * lineSide / 2 + CGFloat(y) * lineSide
@@ -58,6 +63,35 @@ class BoardView: UIView {
         changingColor(color: color).setFill()
         cube1.fill()
         cube1.stroke()
+    }
+    
+    func drawTopface() {
+        drawTopParallelogram(x: 0, y: 0, color: topFace[0])
+        drawTopParallelogram(x: 1, y: 0, color: topFace[1])
+        drawTopParallelogram(x: 2, y: 0, color: topFace[2])
+
+        drawTopParallelogram(x: 0, y: 1, color: topFace[3])
+        drawTopParallelogram(x: 1, y: 1, color: topFace[4])
+        drawTopParallelogram(x: 2, y: 1, color: topFace[5])
+        
+        drawTopParallelogram(x: 0, y: 2, color: topFace[6])
+        drawTopParallelogram(x: 1, y: 2, color: topFace[7])
+        drawTopParallelogram(x: 2, y: 2, color: topFace[8])
+    }
+    
+    func drawTopParallelogram(x: Int, y: Int, color: CubeColor) {
+        let wrongPointX0 = oigX + lineSide + CGFloat(x) * lineSide - CGFloat(y) * lineSide / 2
+        let wrongPointY0 = oigY - lineSide + CGFloat(y) * lineSide / 2
+        
+        let cube2 = UIBezierPath()
+        cube2.move(to: CGPoint(x: wrongPointX0, y: wrongPointY0))
+        cube2.addLine(to: CGPoint(x: wrongPointX0 + lineSide / 2, y: wrongPointY0 - lineSide / 2))
+        cube2.addLine(to: CGPoint(x: wrongPointX0 + lineSide + lineSide / 2, y: wrongPointY0 - lineSide / 2))
+        cube2.addLine(to: CGPoint(x: wrongPointX0 + lineSide, y: wrongPointY0))
+        cube2.close()
+        changingColor(color: color).setFill()
+        cube2.fill()
+        cube2.stroke()
     }
     
     func changingColor(color: CubeColor) -> UIColor {
@@ -80,6 +114,5 @@ class BoardView: UIView {
         
         return realColor
     }
-   
 
 }
