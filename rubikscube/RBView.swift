e0fe7ee6f14bd52845f58ad9da79afeb67e5315c
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2021-04-12 10:50:25 -0400

    we cleaned the code

--- rubikscube/RBView.swift
+++ rubikscube/RBView.swift
@@ -22,53 +22,8 @@ class RBView: UIView {
         middle = CGPoint(x: bounds.width/2, y: bounds.height/2)
         h = sin(30/180 * CGFloat.pi) * hypo // side corner to middle
         b = cos(30/180 * CGFloat.pi) * hypo // middle to edge
-        drawCube()
+
         drawFaces()
-    }
-    
-    func drawCube() {
-        let path = UIBezierPath()
-        let tfLeftSide = CGPoint(x: middle.x - b, y: middle.y - h)
-        let tfRightSide = CGPoint(x: middle.x + b, y: middle.y - h)
-        path.move(to: middle)
-        path.addLine(to: CGPoint(x: middle.x, y: middle.y + hypo))
-        path.addLine(to: CGPoint(x: middle.x + b, y: middle.y + hypo - h))
-        path.addLine(to: CGPoint(x: middle.x + b, y: middle.y - h))
-        path.close()
-        
-        path.move(to: middle)
-        path.addLine(to: CGPoint(x: middle.x, y: middle.y + hypo))
-        path.addLine(to: CGPoint(x: middle.x - b, y: middle.y + hypo - h))
-        path.addLine(to: CGPoint(x: middle.x - b, y: middle.y - h))
-        path.close()
-        
-        path.move(to: middle)
-        path.addLine(to: tfRightSide)
-        path.addLine(to: CGPoint(x: middle.x, y: middle.y - hypo))
-        path.addLine(to: tfLeftSide)
-        path.close()
-        
-        for i in 0 ..< 2 {
-            // right face vertical
-            path.move(to: CGPoint(x: middle.x + b/3 * CGFloat(i + 1), y: middle.y - h/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: middle.x + b/3 * CGFloat(i + 1), y: middle.y + hypo - h/3 * CGFloat(i + 1)))
-            // right face horizontal
-            path.move(to: CGPoint(x: middle.x, y: middle.y + hypo/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: tfRightSide.x, y: tfRightSide.y + hypo/3 * CGFloat(i + 1)))
-            // left face vertical
-            path.move(to: CGPoint(x: middle.x - b/3 * CGFloat(i + 1), y: middle.y - h/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: middle.x - b/3 * CGFloat(i + 1), y: middle.y + hypo - h/3 * CGFloat(i + 1)))
-            // left face horizontal
-            path.move(to: CGPoint(x: middle.x, y: middle.y + hypo/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: tfLeftSide.x, y: tfLeftSide.y + hypo/3 * CGFloat(i + 1)))
-            // top face left
-            path.move(to: CGPoint(x: middle.x + b/3 * CGFloat(i + 1), y: middle.y - h/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: tfLeftSide.x + b/3 * CGFloat(i + 1), y: tfLeftSide.y - h/3 * CGFloat(i + 1)))
-            // top face right
-            path.move(to: CGPoint(x: middle.x - b/3 * CGFloat(i + 1), y: middle.y - h/3 * CGFloat(i + 1)))
-            path.addLine(to: CGPoint(x: tfRightSide.x - b/3 * CGFloat(i + 1), y: tfLeftSide.y - h/3 * CGFloat(i + 1)))
-        }
-        path.stroke()
     }
     
     func drawRFCell(color: UIColor, index: Int) {
