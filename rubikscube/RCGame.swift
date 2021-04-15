e79f84a16b4a1701f0ba1ca1ace079142276e863
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2021-04-15 10:53:57 -0400

    we made the ffc rotation work

--- rubikscube/RCGame.swift
+++ rubikscube/RCGame.swift
@@ -28,5 +28,21 @@ struct RCGame {
         ff[3] = ff[7]
         ff[7] = ff[5]
         ff[5] = a
+        
+        a = uf[6]
+        uf[6] = lf[8]
+        lf[8] = df[2]
+        df[2] = rf[0]
+        rf[0] = a
+        a = uf[7]
+        uf[7] = lf[5]
+        lf[5] = df[1]
+        df[1] = rf[3]
+        rf[3] = a
+        a = uf[8]
+        uf[8] = lf[2]
+        lf[2] = df[0]
+        df[0] = rf[6]
+        rf[6] = a
     }
 }
