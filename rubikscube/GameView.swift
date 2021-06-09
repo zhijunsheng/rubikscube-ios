6ef12a10d4523e6e25de37250b215e5abd8c334d
Andy Yan <andy@Andys-MacBook-Air.local>
2021-06-09 18:26:56 -0400

    we moved the box to the middle

--- rubikscube/GameView.swift
+++ rubikscube/GameView.swift
@@ -9,22 +9,23 @@
 import UIKit
 
 class GameView: UIView {
-
+    let anchorX: CGFloat = 125
+    let anchorY: CGFloat = 250
     
     override func draw(_ rect: CGRect) {
         let pencil = UIBezierPath()
         
-        pencil.move(to: CGPoint(x: 100, y: 200))
-        pencil.addLine(to: CGPoint(x: 400, y: 200))
-        pencil.addLine(to: CGPoint(x: 400, y: 500))
-        pencil.addLine(to: CGPoint(x: 100, y: 500))
+        pencil.move(to: CGPoint(x: anchorX, y: anchorY))
+        pencil.addLine(to: CGPoint(x: anchorX + 300, y: anchorY))
+        pencil.addLine(to: CGPoint(x: anchorX + 300, y: anchorY + 300))
+        pencil.addLine(to: CGPoint(x: anchorX, y: anchorY + 300))
         pencil.close()
-        pencil.addLine(to: CGPoint(x: 325, y: 100))
-        pencil.addLine(to: CGPoint(x: 600, y: 100))
-        pencil.addLine(to: CGPoint(x: 400, y: 200))
-        pencil.addLine(to: CGPoint(x: 400, y: 500))
-        pencil.addLine(to: CGPoint(x: 600, y: 375))
-        pencil.addLine(to: CGPoint(x: 600, y: 100))
+        pencil.addLine(to: CGPoint(x: anchorX + 225, y: anchorY - 100))
+        pencil.addLine(to: CGPoint(x: anchorX + 500, y: anchorY - 100))
+        pencil.addLine(to: CGPoint(x: anchorX + 300, y: anchorY))
+        pencil.addLine(to: CGPoint(x: anchorX + 300, y: anchorY + 300))
+        pencil.addLine(to: CGPoint(x: anchorX + 500, y: anchorY + 175))
+        pencil.addLine(to: CGPoint(x: anchorX + 500, y: anchorY - 100))
         
         pencil.stroke()
     }
