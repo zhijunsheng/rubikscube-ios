fe84dfe81c1cb7f76d9eb9fedb97b2c4a759eeb9
Harry Shen <onetrick@harrys-mbp.myfiosgateway.com>
2021-08-28 18:02:22 -0400

    Created View

---
+++ rubikscube/CubeView.swift
@@ -0,0 +1,21 @@
+//
+//  CubeView.swift
+//  rubikscube
+//
+//  Created by Harry Shen on 8/28/21.
+//  Copyright © 2021 Gold Thumb Inc. All rights reserved.
+//
+
+import UIKit
+
+class CubeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
