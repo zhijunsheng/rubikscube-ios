c4e5ab15c2df16110a65b0a1a6a5879344bde440
Kenneth Wu <kennethwu@Kenneths-Macbook-Pro.local>
2022-01-10 16:54:15 -0500

    today I made the 3d rubixcube.

--- rubikscube/RubiksView.swift
+++ rubikscube/RubiksView.swift
@@ -9,13 +9,56 @@
 import UIKit
 
 class RubiksView: UIView {
-
+    
+    let gx: CGFloat = 50
+    let gy: CGFloat = 200
+    let cell: CGFloat = 100
+    
     override func draw(_ rect: CGRect) {
         let pencil = UIBezierPath()
-        pencil.move(to: CGPoint(x: 50, y: 50))
-        pencil.addLine(to: CGPoint(x: 50, y: 100))
-        pencil.stroke()
         
+        //front face
+        
+        //horizontal lines
+        for i in 0 ..< 4 {
+            pencil.move(to: CGPoint(x: gx + cell * CGFloat(i), y: gy))
+            pencil.addLine(to: CGPoint(x: gx + cell * CGFloat(i), y: gy + cell * 3))
+        }
+        //vertical lines
+        for i in 0 ..< 4 {
+            pencil.move(to: CGPoint(x: gx, y: gy + cell * CGFloat(i)))
+            pencil.addLine(to: CGPoint(x: gx + cell * 3, y: gy + cell * CGFloat(i)))
+            
+        }
+        
+        //top face
+        for i in 0 ..< 4 {
+            pencil.move(to: CGPoint(x: gx + cell / 2 + cell * CGFloat(i), y: gy - cell / 2))
+            pencil.addLine(to: CGPoint(x: gx + cell * CGFloat(i), y: gy ))
+            
+        }
+        
+        pencil.move(to: CGPoint(x: gx + cell / 2, y: gy - cell / 2))
+        pencil.addLine(to: CGPoint(x: gx + cell/2 + cell * 3, y: gy - cell / 2))
+        
+        pencil.move(to: CGPoint(x: gx + cell / 6, y: gy - cell / 6))
+        pencil.addLine(to: CGPoint(x: gx + cell / 6 + cell * 3 , y: gy - cell / 6))
+        
+        pencil.move(to: CGPoint(x: gx + cell / 3, y: gy - cell / 3))
+        pencil.addLine(to: CGPoint(x: gx + cell / 3 + cell * 3 , y: gy - cell / 3))
+        
+        //right face
+        
+        for i in 0 ..< 3 {
+            pencil.move(to: CGPoint(x: gx + cell * 3, y: gy + cell * 3 - cell * CGFloat(i)))
+            pencil.addLine(to: CGPoint(x: gx + cell * 3 + cell / 2, y: gy + cell * 3 - cell / 2 - cell * CGFloat(i)))
+        }
+        
+        for i in 0 ..< 3 {
+            pencil.move(to: CGPoint(x: gx + cell * 3 + cell / 6 * CGFloat(i + 1), y: gy - cell / 6 * CGFloat(i + 1)))
+            pencil.addLine(to: CGPoint(x: gx + cell * 3 + cell / 6 * CGFloat(i + 1), y: gy + cell * 3 - cell / 6 * CGFloat(i + 1)))
+        }
+        
+        pencil.stroke()
     }
-
 }
