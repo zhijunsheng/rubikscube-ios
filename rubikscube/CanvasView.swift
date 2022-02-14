57595ff376a067afe9fe7409711fcac1e50a17d1
Lambert Lin <lambertsixianlin@gmail.com>
2022-02-14 21:55:17 +0800

    We made the code for up face work.

--- rubikscube/CanvasView.swift
+++ rubikscube/CanvasView.swift
@@ -19,9 +19,34 @@ class CanvasView: UIView {
         rightFace()
         
         drawFrontFaceSquareAt(col: 0, row: 0, color: #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1))
-        drawFrontFaceSquareAt(col: 1, row: 2, color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
-        drawFrontFaceSquareAt(col: 2, row: 1, color: #colorLiteral(red: 0.9999304414, green: 0, blue: 0, alpha: 1))
-        drawFrontFaceSquareAt(col: 2, row: 2, color: #colorLiteral(red: 1, green: 0.9612814784, blue: 0, alpha: 1))
+        drawFrontFaceSquareAt(col: 0, row: 1, color: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
+        drawFrontFaceSquareAt(col: 0, row: 2, color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        
+        drawFrontFaceSquareAt(col: 1, row: 0, color: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
+        drawFrontFaceSquareAt(col: 1, row: 1, color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        drawFrontFaceSquareAt(col: 1, row: 2, color: #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1))
+        
+        drawFrontFaceSquareAt(col: 2, row: 0, color: #colorLiteral(red: 0.9999304414, green: 0, blue: 0, alpha: 1))
+        drawFrontFaceSquareAt(col: 2, row: 1, color: #colorLiteral(red: 1, green: 0.9612814784, blue: 0, alpha: 1))
+        drawFrontFaceSquareAt(col: 2, row: 2, color: #colorLiteral(red: 1, green: 0, blue: 0.01270750538, alpha: 1))
+        
+        drawUpperFaceSquareAt(col: 0, row: 0, color: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))
+    }
+    
+    func drawUpperFaceSquareAt(col: Int, row: Int, color: UIColor) {
+        let x = gridX + CGFloat(col) * cellSide + cellSide/6 * CGFloat(3 - row)
+        let y = gridY + CGFloat(row - 3) * cellSide/6
+        
+        let pencil = UIBezierPath()
+        pencil.move(to: CGPoint(x: x, y: y))
+        pencil.addLine(to: CGPoint(x: x - cellSide/6, y: y + cellSide/6))
+        pencil.addLine(to: CGPoint(x: x - cellSide/6 + cellSide, y: y + cellSide/6))
+        pencil.addLine(to: CGPoint(x: x + cellSide, y: y))
+        pencil.close()
+        color.setFill()
+        pencil.fill()
+        pencil.stroke()
+        
     }
     
     func drawFrontFaceSquareAt(col: Int, row: Int, color: UIColor) {
@@ -37,7 +62,7 @@ class CanvasView: UIView {
     }
     
     func upperFace() {
-        
+
         let pencil = UIBezierPath()
         for i in 0..<4 {
             pencil.move(to: CGPoint(x: gridX + cellSide * CGFloat(i), y: gridY))
