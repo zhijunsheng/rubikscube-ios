ba9a9f0db7ecdeb85c0adbac7be2f8cac2ed20fc
jichengye <jichengye@jichengyedeMacBook-Pro.local>
2022-02-18 20:11:51 +0800

    Square is flexible.

--- RubiksCubeView.swift
+++ RubiksCubeView.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class RubiksCubeView: UIView {
     
+    var cellSide: CGFloat = 100
+    var gridX: CGFloat = 200
+    var gridY: CGFloat = 300
+
     override func draw(_ rect: CGRect) {
         let qwer = UIBezierPath()
-        qwer.move(to: CGPoint(x: 100, y: 100))
-        qwer.addLine(to: CGPoint(x: 500, y: 300))
+        for i in 0 ..< 4 {
+            qwer.move(to: CGPoint(x: gridX, y: gridY + cellSide * CGFloat(i)))
+            qwer.addLine(to: CGPoint(x: gridX + 3 * cellSide, y: gridY + cellSide * CGFloat(i)))
+        }
+        for i in 0 ..< 4 {
+            qwer.move(to: CGPoint(x: gridX + cellSide * CGFloat(i), y: gridY))
+            qwer.addLine(to: CGPoint(x: gridX + cellSide * CGFloat(i), y: gridY + 3 * cellSide))
+        }
         qwer.stroke()
     }
-    
 }
