a0d4e0f20750b671f74b10674003180cfa5f46ca
ethan <ethanyiyaozhang@gmail.com>
2022-03-07 20:31:02 +0800

    We created function  drawFrontFace.

--- rubikscube/BoardView.swift
+++ rubikscube/BoardView.swift
@@ -1,9 +1,28 @@
 import UIKit
 
 class BoardView: UIView {
-
+    
+    let cubeX: CGFloat = 150
+    let cubeY: CGFloat = 250
+    let cellSide: CGFloat = 100
+    
     override func draw(_ rect: CGRect) {
+        drawFrontFace()
         
+    }
+    
+    func drawFrontFace() {
+        let pen = UIBezierPath()
         
+        for i in 0..<4 {
+            pen.move(to: CGPoint(x: cubeX, y: cubeY + CGFloat(i) * cellSide))
+            pen.addLine(to: CGPoint(x: cubeX + 3 * cellSide, y: cubeY + CGFloat(i) * cellSide))
+        }
+        
+        for i in 0..<4 {
+            pen.move(to: CGPoint(x: cubeX + CGFloat(i) * cellSide, y: cubeY))
+            pen.addLine(to: CGPoint(x: cubeX + CGFloat(i) * cellSide, y: cubeY + 3 * cellSide))
+        }
+        pen.stroke()
     }
  }
